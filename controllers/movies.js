9008c108e4576f166d556f929e2bab38099df78a
Francisco Javier Diaz Martin <fjdiaz@stratio.com>
2018-08-06 11:56:12 +0200

    Add fethMovies to home page

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,5 +1,13 @@
 const MovieSchema = require('../models/Movie.js');
 module.exports.controller = (app) => {
+  // fetch all movies
+  app.get('/movies', (req, res) => {
+    MovieSchema.find({}, 'name description release_year genre', (err, movies) => {
+      if(err) { console.log(err); }
+      res.send({ movies });
+    });
+  });
+
   // add a new movie
   app.post('/movies', (req, res) => {
     const newMovie = new MovieSchema({
